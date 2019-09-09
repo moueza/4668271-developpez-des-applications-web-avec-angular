63d85f66c1905680cdb3bb7208ce9c80ec41e004
moueza <mouezapeter@gmail.com>
2019-09-09 20:40:57 +0200

    Closes #3 p4c4 video12mn33 firebase linking

--- bookshelves/src/app/app.component.ts
+++ bookshelves/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+  constructor(){
+
+    var firebaseConfig = {
+      apiKey: "AIzaSyAmxHDD91CZACxUf5_04_rnOy2Om2LDTg0",
+      authDomain: "alls-authenth.firebaseapp.com",
+      databaseURL: "https://alls-authenth.firebaseio.com",
+      projectId: "alls-authenth",
+      storageBucket: "alls-authenth.appspot.com",
+      messagingSenderId: "385739580962",
+      appId: "1:385739580962:web:b6c8a2d1d1de74b4fdcc7b"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+  }
+
 }
