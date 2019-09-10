d466e5f0f2b0308b9fc35cbc803919178d498e1b
moueza <mouezapeter@gmail.com>
2019-09-10 15:39:30 +0200

    Closes #7 p4c4 video 29mn35 Firebase/Authentification/Header ; Resolves #8 p4c4 video 34mn34 Firebase/Authentification/Guarded routes ; human tested OK ; no red

--- bookshelves/src/app/app.module.ts
+++ bookshelves/src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
-import { BookListComponent } from './book-list/book-list.component';
-import { SingleBookComponent } from './book-list/single-book/single-book.component';
-import { BookFormComponent } from './book-list/book-form/book-form.component';
-import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {SigninComponent} from './auth/signin/signin.component';
+import {BookListComponent} from './book-list/book-list.component';
+import {SingleBookComponent} from './book-list/single-book/single-book.component';
+import {BookFormComponent} from './book-list/book-form/book-form.component';
+import {HeaderComponent} from './header/header.component';
 import {BooksService} from './services/books.service';
 import {AuthGuardService} from './services/auth-guard.service';
 import {AuthService} from './services/auth.service';
@@ -17,15 +17,17 @@ import {HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
 
 
-const appRoutes: Routes=[
-  {path:'auth/signup',component: SignupComponent},
-  {path:'auth/signin',component: SigninComponent},
-  {path:'books',component: BookListComponent},
-  {path:'books/new',component: BookFormComponent},
-  {path:'books/view/:id',component: SingleBookComponent}
+const appRoutes: Routes = [
+  {path: 'auth/signup', component: SignupComponent},
+  {path: 'auth/signin', component: SigninComponent},
+  {path: 'books', canActivate: [AuthGuardService], component: BookListComponent},
+  {path: 'books/new', canActivate: [AuthGuardService], component: BookFormComponent},
+  {path: 'books/view/:id', canActivate: [AuthGuardService], component: SingleBookComponent},
 
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: '**', redirectTo: 'books'}
+];
 
-]
 @NgModule({
   declarations: [
     AppComponent,
@@ -51,4 +53,5 @@ const appRoutes: Routes=[
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
